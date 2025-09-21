c96948e56e9873d2aa4dc63a020e9833bee7cd25
genario100 <76125969+genario100@users.noreply.github.com>
2025-09-21 07:50:23 -0300

    Adicionar arquivos de projeto.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "login")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "login.Pages")]
